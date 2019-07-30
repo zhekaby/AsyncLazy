efd14848b78a526357308db6485665f6a950c32e
zhekaby <zhekaby@gmail.com>
2019-07-30 16:07:40 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 ApiKey=$1
 Source=$2
-
-curl -L -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+curl -L -o nuget.exe "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
 nuget pack ./LazyAsync/LazyAsync.nuspec -Verbosity detailed
 nuget push ./LazyAsync/LazyAsync.NET.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
