5a4f3e6879ba403d9333d4ef1e633ad65e4a7101
zhekaby <zhekaby@gmail.com>
2019-07-30 17:13:53 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ set -e
 API_KEY=$1
 VERSION=$2
 
-dotnet pack LazyAsync/LazyAsync.csproj -c Release -o out /p:NuspecFile=LazyAsync.nuspec -p:PackageVersion=$VERSION
-
+dotnet pack LazyAsync/LazyAsync.csproj -c Release -o out -p:PackageVersion=$VERSION
 dotnet nuget push LazyAsync/out/LazyAsync.NET.$VERSION.nupkg --api-key $API_KEY --source https://api.nuget.org/v3/index.json
