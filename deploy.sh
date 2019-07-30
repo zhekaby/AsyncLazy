2e3dbb8c784ec0c791138e7c6d6137873396d606
zhekaby <zhekaby@gmail.com>
2019-07-30 15:16:06 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ ApiKey=$1
 Source=$2
 
 nuget pack ./LazyAsync/LazyAsync.nuspec -Verbosity detailed
-nuget push ./LazyAsync/LazyAsync.NET.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json
+nuget push ./LazyAsync/LazyAsync.NET.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
