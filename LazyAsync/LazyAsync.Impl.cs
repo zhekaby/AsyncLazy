b955d09e702aa56ee5cdb59637e983cf882c17cc
zhekaby <zhekaby@gmail.com>
2019-07-30 18:57:26 +0300

    naming fix (#6)

--- LazyAsync/LazyAsync.Impl.cs
+++ LazyAsync/LazyAsync.Impl.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace LazyAsync
+namespace S4B.LazyAsyncNET
 {
     public class LazyAsync<T>
     {
