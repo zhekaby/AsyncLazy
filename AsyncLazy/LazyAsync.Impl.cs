7e7b471477a1776d1e23fc7dda5113eef20e862f
zhekaby <zhekaby@gmail.com>
2019-07-30 13:53:28 +0300

    initial commit

---
+++ AsyncLazy/LazyAsync.Impl.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyAsync
+{
+    public class LazyAsync<T>
+    {
+        class Data
+        {
+            public long RenewAt, ExpiresAt;
+            public Task<T> Current, Pending;
+        }
+
+        private Data _data = new Data();
+
+        public Task<T> Task
+        {
+            get
+            {
+                var now = DateTime.UtcNow.Ticks;
+                var data = _data;
+
+                for (; ; )
+                {
+                    if (data.ExpiresAt >= now)
+                    {
+                        if (data.RenewAt < now && data.Pending == data.Current)
+                        {
+                            var tcs = new TaskCompletionSource<T>();
+                            var pending = new Data
+                            {
+                                RenewAt = data.RenewAt,
+                                ExpiresAt = data.ExpiresAt,
+                                Current = data.Current,
+                                Pending = tcs.Task
+                            };
+
+                            var xchgResult = Interlocked.CompareExchange(ref _data, pending, data);
+
+                            if (xchgResult == data)
+                                Evaluate(tcs);
+                        }
+
+                        return data.Current;
+                    }
+
+                    if (data.Pending != data.Current)
+                    {
+                        return data.Pending;
+                    }
+                    else
+                    {
+                        var tcs = new TaskCompletionSource<T>();
+
+                        var pending = new Data
+                        {
+                            RenewAt = data.RenewAt,
+                            ExpiresAt = data.ExpiresAt,
+                            Current = data.Current,
+                            Pending = tcs.Task
+                        };
+
+                        var xchgResult = Interlocked.CompareExchange(ref _data, pending, data);
+
+                        if (xchgResult != data)
+                        {
+                            data = xchgResult;
+                            continue;
+                        }
+
+                        Evaluate(tcs, true);
+
+                        return tcs.Task;
+                    }
+                }
+            }
+        }
+
+        private readonly Func<Task<(T value, DateTime renewAt, DateTime expiresAt)>> eval;
+
+        public LazyAsync(
+           Func<Task<(T value, DateTime renewAt, DateTime expiresAt)>> eval,
+           Task<(T value, DateTime renewAt, DateTime expiresAt)> initial = null, string name = null)
+        {
+            if (initial != null)
+            {
+                this.eval = () => initial;
+                var tcs = new TaskCompletionSource<T>();
+
+                _data = new Data
+                {
+                    RenewAt = 0,
+                    ExpiresAt = 0,
+                    Current = null,
+                    Pending = tcs.Task
+                };
+
+                Evaluate(tcs);
+            }
+
+            this.eval = eval;
+        }
+
+        async void Evaluate(TaskCompletionSource<T> tcs, bool blocking = false)
+        {
+            try
+            {
+                var result = await eval();
+
+                var pending = new Data
+                {
+                    RenewAt = result.renewAt.Ticks,
+                    ExpiresAt = result.expiresAt.Ticks,
+                    Current = tcs.Task,
+                    Pending = tcs.Task
+                };
+
+                var xchgResult = _data;
+                Data data;
+
+                do
+                {
+                    data = xchgResult;
+
+                    if (data.Pending != tcs.Task)
+                        break;
+
+                    xchgResult = Interlocked.CompareExchange(ref _data, pending, data);
+
+                } while (data != xchgResult);
+
+                tcs.SetResult(result.value);
+            }
+            catch (Exception ex)
+            {
+                var data = _data;
+
+                if (data.Current == tcs.Task)
+                {
+                    Interlocked.CompareExchange(ref _data, new Data(), data);
+                }
+                else if (data.Pending == tcs.Task)
+                {
+                    Interlocked.CompareExchange(ref _data, new Data
+                    {
+                        RenewAt = data.RenewAt,
+                        ExpiresAt = data.ExpiresAt,
+                        Current = data.Current,
+                        Pending = data.Current
+                    }, data);
+                }
+
+                tcs.SetException(ex);
+            }
+        }
+
+        public TaskAwaiter<T> GetAwaiter() => Task.GetAwaiter();
+
+        public void Invalidate() => _data = new Data();
+
+
+    }
+}
---
+++ AsyncLazy/LazyAsync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LazyAsync
+{
+    public class LazyAsync
+    {
+        public static LazyAsync<T> Create<T>(
+            Func<Task<(T value, DateTime renewAt, DateTime expiresAt)>> eval,
+            Task<(T value, DateTime renewAt, DateTime expiresAt)> initial = null)
+        {
+            return new LazyAsync<T>(eval, initial);
+        }
+
+        public static LazyAsync<T> Create<T>(
+            TimeSpan renewAge,
+            TimeSpan expiresAge,
+            Func<Task<T>> eval)
+        {
+            return new LazyAsync<T>(() => eval().ContinueWith(t => (t.Result, DateTime.UtcNow + renewAge, DateTime.UtcNow + expiresAge), TaskContinuationOptions.ExecuteSynchronously), null, eval.Method.ToString());
+        }
+
+        public static LazyAsync<T> Create<T>(
+            TimeSpan renewAge,
+            Func<Task<T>> eval)
+        {
+            return new LazyAsync<T>(() => eval().ContinueWith(t => (t.Result, DateTime.UtcNow + renewAge, DateTime.MaxValue), TaskContinuationOptions.ExecuteSynchronously), null, eval.Method.ToString());
+        }
+    }
+
+    
+}
